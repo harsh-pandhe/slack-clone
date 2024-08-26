31c00087eca8ebaeb490c799a32bb19beb2529f5
Harsh Pandhe <harshpandhehome@gmail.com>
2024-08-27 04:41:31 +0530

    Configuration Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Harsh Pandhe</h1>
-    </main>
+    <Button variant={"slack"} size={"lg"}> Click me </Button>
   );
 }
